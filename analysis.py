fbf74322d54482b4d43b3218fed5fa6d4d9c72af
Nikhil Saxena <saxena.nik2@gmail.com>
2016-03-01 02:16:32 +0530

    optimized with 82% accuracy at max

--- analysis.py
+++ analysis.py
@@ -1,9 +1,13 @@
 import json
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 import random
+import re
+import matplotlib.pyplot as plt
 
 stop = stopwords.words('english')
+stemmer = SnowballStemmer("english", ignore_stopwords=True)
 positiveTweets = []
 negativeTweets = []
 ignoreTweets = []
@@ -16,7 +20,8 @@ def initializeLists():
     tweetsData = open('tweetsSentiment.json','r')
     tweetsList = list(tweetsData)
     for line in tweetsList:
-        tweet = json.loads(line)
+        tweet = json.loads(line)        
+        tweet['text'] = applyStemmerToTweet(tweet['text'])
         if tweet['sentiment'] == 'p':
             positiveTweets.append((tweet['text'],tweet['sentiment']))
         elif tweet['sentiment'] == 'n':
@@ -30,26 +35,31 @@ def extract_words(appendTo,fromTweets):
         words_filtered = [word.lower() for word in words.split() if len(word)>2]
         appendTo.append((words_filtered, sentiment))
 
-def classifier():
+def classifier(noOfFeatures):
     allWords = []
     for (words,sentiment) in trainingTweetsWords:
         allWords.extend(words)
-    #wordList = nltk.FreqDist(allWords)
-    #print sorted(wordList.iteritems(),key=lambda(k,v):(v,k),reverse=True)
-    #remove= ['and','the','have','has','@nav_prdp:', ]
-    wordList = set(allWords) - set(stop)
-    wordList = list(wordList)
+    wordList = nltk.FreqDist(allWords)
+    wordList = sorted(wordList.iteritems(),key=lambda(k,v):(v,k),reverse=True)
+    wordList = [word for (word,freq) in wordList]
+    #print wordList[:10]
+    #print set(wordList)
+    #wordList = set(wordList) - set(stop)
+    #wordList = list(wordList)
     #print len(wordList)
-    #print wordList
+    #print wordList[:10]
     for word in wordList:
-        if 'http' in word:
+        if word in stop:
             wordList.remove(word)
-        if '@' in word:
+        elif re.search(r'http[\w:]+',word):
             wordList.remove(word)
-        if '\u' in word:
+        elif re.search(r'[\w:?]*@\w*',word):
             wordList.remove(word)
+        elif re.search(r'\\u[\w:@.]*',word):
+            wordList.remove(word)          
+            
     #print len(wordList)
-    return wordList
+    return wordList[:noOfFeatures]
 
 def extract_features(tweetWords):
     tweetWords = set(tweetWords)
@@ -79,30 +89,34 @@ def seperateTestTraining(i):
         count += 1
     testTweets.extend(positiveTweets+negativeTweets+ignoreTweets)
 
+def applyStemmerToTweets(tweets):
+    stemmedTweets = []
+    for tweet in tweets:
+        stemmedTweets.append(applyStemmerToTweet(tweet))
+    return stemmedTweets
+
+def applyStemmerToTweet(tweet):
+    return ' '.join([stemmer.stem(word) for word in tweet.split()])
 
 def main():
     initializeLists()
     i=10
     seperateTestTraining(i)
-    print len(trainingTweets)
-    print len(testTweets)
-    #trainingTweets.extend(positiveTweets[:i] + negativeTweets[:i] + ignoreTweets[:i])
-    #testTweets.extend(positiveTweets[i:] + negativeTweets[i:] + ignoreTweets[i:])
     extract_words(trainingTweetsWords, trainingTweets)
-    wordFeatures.extend(classifier())
+    noOfFeatures = 91    
+    wordFeatures.extend(classifier(noOfFeatures))
     trainingSet = nltk.classify.apply_features(extract_features, trainingTweetsWords)
     Classifier = nltk.NaiveBayesClassifier.train(trainingSet)
-    #print Classifier.show_most_informative_features(32)
     #checking accuracy
     count = 0
     for (tweet,sentiment) in testTweets:
         prediction = Classifier.classify(extract_features(tweet.split()))
-        #print prediction,sentiment
         if prediction == sentiment:
             count += 1
-    #print count
-    print 'accuracy %d = '%i + str(count/float(len(testTweets)))
+    accuracy = count/float(len(testTweets))
+    print 'accuracy using %d*3 trainingTweets = '%i + str(accuracy)
     sampleTweet  = 'accha hua warna paise se wishwaas uth jaata'
+    sampleTweet = applyStemmerToTweet(sampleTweet)
     print 'sample prediction: ' + Classifier.classify(extract_features(sampleTweet.split()))
 
 if __name__ == '__main__':
